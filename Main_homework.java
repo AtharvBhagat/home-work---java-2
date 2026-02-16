4083cf76909528505b14193c1fd1be815dacedce
AtharvBhagat <159274676+AtharvBhagat@users.noreply.github.com>
2026-02-16 09:12:20 +0530

    Add files via upload

---
+++ Main_homework.java
@@ -0,0 +1,17 @@
+package lesson_2;
+
+import java.util.Scanner;
+
+public class Main_homework {
+    public static void main(String[] args){
+        int a=4;
+        int b=2024;
+        int c=2025;
+
+        System.out.println("2024 is a leap year " + (a/b));
+        System.out.println("2025 is not a leap year" + (a/c));
+
+
+
+    }
+}
